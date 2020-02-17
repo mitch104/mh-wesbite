3d20ff8345b50e357421d8921481b571644a0374
mitch104 <mitch_10_4@hotmail.com>
2020-02-17 20:45:23 +0000

    Modify header and change footer links

--- gridsome-portfolio-starter/src/components/SearchInput.vue
+++ gridsome-portfolio-starter/src/components/SearchInput.vue
@@ -5,7 +5,7 @@
     <div class="relative w-80">
       <input
         type="text"
-        placeholder="Search (Press  &quot;/&quot; to focus)"
+        placeholder="Search"
         class="bg-background-form border border-gray-500 rounded-full px-4 pl-10 py-2 outline-none focus:border-green-500 w-80"
         v-model="query"
         @input="softReset"
@@ -150,4 +150,3 @@ export default {
     opacity: 0;
   }
 </style>
-
